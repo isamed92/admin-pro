24b36d7f2078b3f7a186edc83871a2a1469e8871
IsaiasPC <isamed92@gmail.com>
2019-07-30 01:05:22 -0300

    modules finished

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+  declarations: [
+    NotpagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NotpagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ]
+})
+export class SharedModule { }
