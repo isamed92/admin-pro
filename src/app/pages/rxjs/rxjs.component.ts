85c1d9c4e94cccc3698d52d587cb5d404fc387a4
IsaiasPC <isamed92@gmail.com>
2019-08-21 01:16:59 -0300

    add observable and retry function

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class RxjsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.regresaObservable()
+      .pipe(retry(2))
+      .subscribe(
+        numero => console.log('subs to', numero),
+        error => console.error('error en el observable', error),
+        () => console.log('el observador termino')
+      );
   }
 
+  ngOnInit() {}
+
+  regresaObservable(): Observable<number> {
+    return new Observable(observer => {
+      let contador = 0;
+      const intervalo = setInterval(() => {
+        contador += 1;
+        observer.next(contador);
+        if (contador === 3) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        if (contador === 2) {
+          // clearInterval(intervalo);
+          observer.error('HELP!!!');
+        }
+      }, 1000);
+    });
+  }
 }
