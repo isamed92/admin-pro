44d0b8a3685f1b7104a839e1177f8d8a61e3ed8e
IsaiasPC <isamed92@gmail.com>
2019-08-22 02:30:39 -0300

    add Metatags and title in page from breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -9,8 +10,20 @@ import { filter, map } from 'rxjs/operators';
 })
 export class BreadcrumbsComponent implements OnInit {
   titulo: string;
-  constructor(private router: Router) {
-    this.getDataRoute().subscribe(data => this.titulo = data.titulo)
+  constructor(
+      private router: Router,
+      private title: Title,
+      private meta: Meta
+    ) {
+    this.getDataRoute().subscribe(data => {
+      this.titulo = data.titulo
+      this.title.setTitle(this.titulo);
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.titulo
+      };
+      this.meta.updateTag(metaTag);
+    });
   }
 
   ngOnInit() {}
